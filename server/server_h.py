e6dee36dc1dc1060ea428b0e0e43ecbdd69e7507
Wh1tew0lf-sea <wh1tew0lf@163.com>
2024-04-25 10:41:39 +0800

    completeserver1.0

--- server/server_h.py
+++ server/server_h.py
@@ -9,7 +9,7 @@ import pickle
 '''
 class Server():
     #初始化服务器
-    def __init__(self, ipaddress, port, net, name, status, timestamp,chain=None):
+    def __init__(self, ipaddress, port, net, name, status, timestamp,Chain=None):
         # super().__init__()
         self.ipaddress = ipaddress  # 区块存储位置
         self.port = port
@@ -17,47 +17,14 @@ class Server():
         self.name = name #服务器名称
         self.status = status  # 状态
         self.timestamp = timestamp  # 上一次更新时间
-        self.chain = chain #chain
+        self.Chain = Chain #chain
 
-
-    def _checkchain(self):
-        # 创建创世交易
-        genesisTransaction = Transaction(self._minerAddress, '0', 0, time.time_ns())
-        # 创建创世区块的Merkle树
-        genesisMerkleTree = MerkleTree([genesisTransaction])
-        # 将创世交易添加到创世区块中
-        self.Blocks.append(Block(genesisMerkleTree, 0, self._initHash))
 
     #导入链
-    def LoadChainpkl(self, ChainPath):
+    def loadChainpkl(self, ChainPath):
         # 将pkl的形式存储链展开，
         with open(ChainPath, 'wb') as f:
             pickle.load(self, f)
-
-    #导入链
-    def expkl(self, pkl):
-        # 校验哈希值是否合法
-        for index in range(self._difficulty):
-            if Hash[index] != '0':
-                return False
-        return True
-
-    #向网络广播新区块
-    def broadcast(self):
-
-    def LoadChain(self, ChainPath):
-        # 将链存到本地，以pkl的形式存储
-        with open(ChainPath, 'wb') as f:
-            pickle.load(self, f)
-
-    def createNewBlock(self, newBlock):
-        # 检查新区块的哈希值是否符合要求
-        if self.checkHash(newBlock.block_hash):
-            self.Blocks.append(newBlock)
-            return True
-        else:
-            return False
-
 
 
 
--- server/server_main.py
+++ server/server_main.py
@@ -1,18 +1,51 @@
 import random
+import time
+
 from base.baseCFG import *
 from base.GCBChainStructure import *
 from base.User import *
 from base.Miner import *
 
-#server 的任务:1. 生成区块 2. 验证交易 3. 验证区块 4. 广播区块 5. 存储区块 6.存储交易 7.接受广播
+'''
+server 的任务:
+1. 与主服务器建立连接 
+2. 接收连接并将ip存储 
+3. 生成新区块验证 
+4. 广播区块 
+5. 接受广播并验证添加区块 
+6. 存储总链
+'''
 import server_h
 
-def server_init(ipaddress, N,status,timestamp,chain,port=8848,name="default"):
-    server = server_h.Server(ipaddress, port, N, name, status, timestamp,chain)
+def server_init(ipaddress, remoteipaddress,status,timestamp,chain,port=8848,name="default"):
+    server = server_h.Server(ipaddress, port, remoteipaddress, name, status, timestamp,chain)
 # 向网络广播本机情况
 
 # 向其他node建立连接请求获取链数据
-def connect_to_nodes():
+def request_to_net():
+    return 0
 
-# 导入区块链
-blockchain = Chain()
+# 接收连接并将ip存储
+def receive_connection(ipaddress, port):
+    return 0
+
+# 挖矿生成新区块
+def mine():
+    return 0
+
+# broadcastBlock
+def broadcastBlock():
+    return 0
+
+
+# 监听模式
+def monitormode():
+    return 0
+
+# 存储
+def storage():
+    return 0
+
+if __name__ == '__main__':
+    time_now = time.time_ns()
+    server_init("127.0.0.1","remoteipaddress",0,time_now)
