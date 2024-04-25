ec34e549004b3d29d1add3708e24b9daa91e440c
Andrew82106 <qq281512383@163.com>
2024-04-25 10:45:00 +0800

    update pathconfig.py ，做了包适配

--- base/pathconfig.py
+++ base/pathconfig.py
@@ -3,46 +3,45 @@ import sys
 
 
 # 定义一个PathCFG类
-class PathCFG:
+class Pathconfig:
     """
-    PathCFG类用于设置项目的路径。
+    Pathconfig类用于设置项目的路径。
 
     Attributes:
-        projectName (str): 项目名称。
+        projectName (str): base文件夹的上一层文件夹的名称
         rootPth (str): 项目根路径。
         basePath (str): 项目基础路径。
-        wheelsPath (str): 项目轮子路径。
         utilsPath (str): 项目工具路径。
 
     Methods:
         _add_sys_pth(): 将项目的相关路径都加入sys.path中。
 
     Example:
-        C = PathCFG()
-
-        print(C.projectName)  # 输出：GCB
+        from base.pathconfig import Pathconfig
+        cfg = Pathconfig()
+        # 路径配置必须先写这两句，后面的就该怎么引用怎么引用
     """
     # 初始化函数
     def __init__(self):
-        # 设置项目名称
-        self.projectName = 'GCB'
+        # 设置包名称
+        self.projectName = 'base'
         # 设置根路径
         self.rootPth = None
         # 获取当前路径
         current_path = os.getcwd()
         # 循环查找项目根路径
         while True:
-            # 如果当前路径下存在项目名称的文件夹，则设置根路径
+            # 如果当前路径下存在"projectName"文件夹，则退出循环
             if os.path.exists(os.path.join(current_path, self.projectName)):
-                self.rootPth = os.path.join(current_path, self.projectName)
+                self.rootPth = current_path
                 break
-            # 获取上一级路径
-            parent_path = os.path.dirname(current_path)
-            # 如果当前路径等于上一级路径，则退出循环
-            if current_path == parent_path:
+
+            # 如果当前路径下不存在"projectName"文件夹，则向上移动一层
+            current_path = os.path.dirname(current_path)
+
+            # 如果当前路径已经到达系统根目录，则退出循环
+            if current_path == '/':
                 break
-            # 更新当前路径
-            current_path = parent_path
         # 如果找到根路径，则打印
         if self.rootPth:
             print(f"Project root path found: {self.rootPth}")
@@ -52,7 +51,6 @@ class PathCFG:
 
         # 一级路径
         self.basePath = os.path.join(self.rootPth, "base")
-        self.wheelsPath = os.path.join(self.rootPth, "Wheels")
 
         # 二级路径
         self.utilsPath = os.path.join(self.basePath, "utils")
@@ -68,7 +66,6 @@ class PathCFG:
         # 将项目的相关路径都加入sys.path中
         sys.path.append(self.rootPth)
         sys.path.append(self.basePath)
-        sys.path.append(self.wheelsPath)
         sys.path.append(self.utilsPath)
         # 将sys.path中的路径依次打印出来
         print("current system path cache:\n" + '*'*40)
@@ -78,4 +75,4 @@ class PathCFG:
 
 
 if __name__ == '__main__':
-    C = PathCFG()
+    C = Pathconfig()
---
+++ client/base/DebugFile.py
@@ -0,0 +1,38 @@
+import random
+from GCBChainStructure import *
+from User import *
+from Miner import *
+
+# 创建账号GodAddress
+userPool = UserPool()
+GodAddress = userPool.addNewUser()
+
+print("God Address:", GodAddress)
+
+# 创建链
+GCBChain = Chain(GodAddress)
+# 输出链信息进行调试
+GCBChain.debugOutputChain()
+# 创建其他账号
+
+evalAddress = userPool.addNewUser()
+print("Eval Address:", evalAddress)
+minerAddress = userPool.addNewUser()
+print("Miner Address:", minerAddress)
+
+# 创建交易
+t0 = Transaction(GodAddress, evalAddress, 0, time.time_ns())
+
+# 挖矿
+m = Miner(minerAddress)
+nonce, newBlock = m.mine(GCBChain.Blocks[-1].data.MTreeLst, GCBChain.Blocks[-1].block_hash, t0)
+
+if GCBChain.createNewBlock(newBlock):
+    print("validate Mining result")
+else:
+    print("invalidate Mining result")
+
+
+# 输出链信息进行调试
+GCBChain.debugOutputChain()
+print("end")
