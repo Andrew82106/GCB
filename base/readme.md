c8b62521aa4280a23d9160adadeac6a17f2e3c43
Andrew82106 <qq281512383@163.com>
2024-04-26 17:07:43 +0800

    尝试编写套接字模块

--- base/readme.md
+++ base/readme.md
@@ -2,6 +2,24 @@
 
 - 定义所需的所有基础类和工具
 - 以包的形式使用base，将编写的代码放在和base同级的位置则可引用
+
+比如：
+
+```text
+- project
+|
+|- base
+|   |- utils
+|   |   |- hashTools.py
+|   |   |- ...
+|   |- pathconfig.py
+|   |- ...
+|   
+|- server.py
+|- client.py
+|- ...
+```
+
 - 引用base中的代码前，先做如下引用以初始化项目路径
 
 ```python
---
+++ base/sockets/Client.py
@@ -0,0 +1,31 @@
+import socket
+import random
+
+
+class client:
+    def __init__(self):
+        self.host = ''
+        self.port = 8848
+        self.ADDR = (self.host, self.port)
+        self.buffsize = 1024
+        self.max_listen = 5
+        self.encoding = 'utf-8'
+
+    def start(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect(self.ADDR)
+            print('connected')
+            cnt = 1
+            while cnt < 10:
+                data = str(random.randint(1, 100))
+                s.send(data.encode(self.encoding))
+                recv_data = s.recv(self.buffsize)
+                print(recv_data)
+                cnt += 1
+            s.close()
+            print("disconnected")
+
+
+if __name__ == '__main__':
+    c = client()
+    c.start()
