995ceea1ad10072d467d6a47c124ca633544ff04
Andrew82106 <qq281512383@163.com>
2024-04-26 16:24:29 +0800

    升级readme，修改一些细节

--- server/base/base/Miner.py
+++ server/base/base/Miner.py
@@ -12,16 +12,21 @@ class Miner:
         defaultChain (Chain): 矿工默认的区块链副本方法。
 
     Methods:
+        _mineMethod(): 私有方法，用于生成随机数。
         mine(preBlocksTLst, preHash, transaction): 挖矿方法，用于生成新的区块。
     """
     def __init__(self, MinerAddress):
         self.address = MinerAddress
         self.defaultChain = Chain('')
 
+    @staticmethod
+    def _mineMethod():
+        return random.randint(1, 100000000)
+
     def mine(self, preBlocksTLst, preHash, transaction):
         print("Mining......")
         while 1:
-            nonce = random.randint(1, 100000000)
+            nonce = self._mineMethod()
             MTree = MerkleTree(preBlocksTLst + [transaction])
             newBlock = Block(MTree, nonce, preHash)
             if self.defaultChain.checkHash(newBlock.block_hash):
